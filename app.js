6db4328e1c56499f1c22d08aa671ba2c7f8feb5f
Kreed <derekandrews@protonmail.com>
2017-10-18 22:57:13 +0000

    committing the code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// require('dotenv').config();
 var Twit = require('twit');
 var config = require('./config');
-var T = new Twit(config);
+var T = Twit(config);
 
 //Setting up a user stream
 var stream = T.stream('user');
@@ -11,7 +10,8 @@ stream.on('follow', followed);
 
 function followed(eventMsg) {
     var screenName = eventMsg.source.screen_name;
-    tweetIt('@' + screenName + ', Welcome and thank you for following us on Twitter! We hope you like our tweets and look forward to sharing interests. Regards from all of us at Andrews Tax Consulting in Dublin.');
+    console.log("Just followed by " + screenName);
+    tweetIt('@' + screenName + '- Thank you for following us on Twitter! Check out our blog www.andrewstax.ie/blog & DM with any questions');
     }
 
 function tweetIt(txt) {
@@ -20,13 +20,15 @@ function tweetIt(txt) {
         status: txt
     };
 
+    console.log(txt);
+    
     T.post('statuses/update', tweet, tweeted);
-
+    
     function tweeted(err, data, response) {
-      if (err) {
-          console.log("Something went wrong");
-      }
-     console.log("It worked");
-}
-
+        if (err) { 
+            console.log("Reply successful" + "\n" + "...............................");
+            } else {
+            console.log("msg sent ok");
+            }
+    }
 }
--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 // config.js
 
+// module.exports = {
+//   consumer_key: process.env.BOT_CONSUMER_KEY,
+//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//   access_token: process.env.BOT_ACCESS_TOKEN,
+//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// };
+
 module.exports = {
-  consumer_key: process.env.BOT_CONSUMER_KEY,
-  consumer_secret: process.env.BOT_CONSUMER_SECRET,
-  access_token: process.env.BOT_ACCESS_TOKEN,
-  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+  consumer_key: 'iLF0GJZdeiEtsJZuQStlpLgoq',
+  consumer_secret: 'KWMjhotkgMNcCy4ITAAOauRRy1UGH9g55kHa3zOXlhMh8DufAT',
+  access_token: '904453598570061824-w0QWYv3FnyVjeozNTfu648Sfn2IhJ9g',
+  access_token_secret: 'dxsiiauHdWZNOuCJaScIvqtKKEXfpNpElq8O9XxPT4uk3'
 };
-
-
