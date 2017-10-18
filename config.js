aafb9303b8e80411f920d8d838b92f30541a2319
Kreed <derekandrews@protonmail.com>
2017-10-18 23:00:02 +0000

    committing the code

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
 // config.js
 
-// module.exports = {
-//   consumer_key: process.env.BOT_CONSUMER_KEY,
-//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
-//   access_token: process.env.BOT_ACCESS_TOKEN,
-//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-// };
-
 module.exports = {
-  consumer_key: 'iLF0GJZdeiEtsJZuQStlpLgoq',
-  consumer_secret: 'KWMjhotkgMNcCy4ITAAOauRRy1UGH9g55kHa3zOXlhMh8DufAT',
-  access_token: '904453598570061824-w0QWYv3FnyVjeozNTfu648Sfn2IhJ9g',
-  access_token_secret: 'dxsiiauHdWZNOuCJaScIvqtKKEXfpNpElq8O9XxPT4uk3'
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 };
